712c93840e4986b4076a0c311effc5dc526502ca
phatphudo <91005753+phatphudo@users.noreply.github.com>
2022-04-11 09:58:21 +0700

    11/4

--- dataset.py
+++ dataset.py
@@ -51,8 +51,7 @@ class eKYCDataset(Dataset):
         target['area'] = torch.as_tensor([anno['area']])
         target['iscrowd'] = torch.as_tensor([anno['iscrowd']])
         target['masks'] = torch.as_tensor([np.array(mask)], dtype=torch.uint8)
-        print(target['masks'].shape)
-        exit(-1)
+
         if self.model_type == 'single':
             target['labels'] = torch.as_tensor([1], dtype=torch.int64)
         elif self.model_type == 'multiple':
