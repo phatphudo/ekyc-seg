b10f6fd5c0a57a124ff847679584d9aad6678898
phatphudo <91005753+phatphudo@users.noreply.github.com>
2022-04-11 16:23:57 +0700

    11/4

--- coco_eval.py
+++ coco_eval.py
@@ -51,9 +51,12 @@ class CocoEvaluator:
             coco_eval.accumulate()
 
     def summarize(self):
+        metrics = {}
         for iou_type, coco_eval in self.coco_eval.items():
-            print(f"IoU metric: {iou_type}")
+            # print(f"IoU metric: {iou_type}")
             coco_eval.summarize()
+            metrics[iou_type] = coco_eval.stats[0]
+        return metrics
 
     def prepare(self, predictions, iou_type):
         if iou_type == "bbox":
--- engine.py
+++ engine.py
@@ -110,6 +110,6 @@ def evaluate(model, data_loader, device):
 
     # accumulate predictions from all images
     coco_evaluator.accumulate()
-    coco_evaluator.summarize()
+    metrics = coco_evaluator.summarize()
     torch.set_num_threads(n_threads)
-    return coco_evaluator
+    return metrics
